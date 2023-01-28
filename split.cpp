571ab40bd276e7f155321d8d073d87c4e8771359
Rina Yang <yangsabr@usc.edu>
2023-01-28 00:45:32 +0000

    final submission

--- split.cpp
+++ split.cpp
@@ -9,8 +9,9 @@ To test your program write a separate .cpp file and #include
 split.h.  **Do NOT add main() to this file**.  When you submit
 the function below should be the only one in this file.
 */
-
+#include <cstddef>
 #include "split.h"
+#include <iostream>
 
 /* Add a prototype for a helper function here if you need */
 
@@ -18,6 +19,25 @@ void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+
+if(in==NULL){
+  evens=nullptr;
+  odds=nullptr;
+  return;
+}
+int val=in->value%2;
+if (val==0){
+  evens=in;
+  Node* n=in->next;
+  in=nullptr;
+  split(n, odds, evens->next);
+}
+if (val!=0){
+  odds=in;
+  Node* n=in->next;
+  in=nullptr;
+  split(n, odds->next, evens);
+}
 }
 
 /* If you needed a helper function, write it here */
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -25,6 +25,127 @@ size_t ULListStr::size() const
 }
 
 // WRITE YOUR CODE HERE
+void ULListStr::pop_back(){   
+  if (head_ == nullptr){ 
+      return;
+  }
+  tail_->last--;
+  if (head_==tail_ && head_->first == head_->last){
+      head_ = nullptr;
+  }
+  else if(head_->first == head_->last){         
+      tail_=tail_->prev;
+      delete tail_->next;
+      tail_->next = nullptr; 
+  }
+    size_--;
+  }
+
+void ULListStr::push_back(const std::string& val){    
+    if (head_ == nullptr){
+      Item* t = new Item();
+      t->val[0] = val;
+      t->last = 1;
+      head_ = t;
+      tail_ = t;
+    }
+    else if (tail_->last == 10){          
+      Item* t = new Item();      
+      t->val[0] = val;
+      t->last = 1;
+      tail_->next = t;
+      t->next = nullptr;
+      t->prev = tail_;
+      tail_=t;
+    }
+    else{
+      tail_->val[tail_->last] = val;
+      tail_->last++;
+    }
+    size_++;               
+  }
+
+void ULListStr::push_front(const std::string& val){     
+  if (head_ == nullptr){
+    Item* t = new Item();
+    t->val[9] = val; 
+    t->last = 10;                 
+    t->first = 9;
+    head_ = t;
+    tail_ = t;
+  }
+  else if(head_->first == 0){
+    Item* t = new Item();
+    t->val[9] = val;
+    t->first = 9; 
+    t->last = 10;                  
+    head_->prev = t;  
+    t->prev = nullptr;            
+    t->next = head_;
+    head_ = t;
+  }
+  else{
+    head_->val[head_->first - 1] = val;
+    head_->first--;
+  }
+  size_++;
+
+}
+
+void ULListStr::pop_front(){       
+  if (head_ == nullptr){
+    return;
+  }
+  head_->first++;
+
+  if (head_==tail_ && head_->last == head_->first){
+      head_ = nullptr;
+  }
+
+  else if (head_->last == head_->first){        
+    head_ = head_->next;
+    delete head_->prev;
+    head_->prev = nullptr;
+
+  }
+size_--;
+}
+
+std::string const & ULListStr::back() const{       
+
+  int p = tail_->last - 1;
+  return tail_->val[p];
+
+}
+
+std::string const & ULListStr::front() const{     
+
+  int p = head_->first;
+  return head_->val[p];
+
+}
+std::string* ULListStr::getValAtLoc(size_t loc) const{         
+  if (head_ == nullptr){
+    return nullptr;
+  }
+  Item* t = nullptr; 
+  t = head_;
+  unsigned int c = 0;             
+  unsigned int in = 0;
+  for (in = head_->first; c!= size_ + head_->first; c++){
+     if (c == loc){           
+      break;
+    }
+    if (in == (t->last) - 1){
+      t = t->next;
+      in = 0;
+    }
+    else{
+      in++;
+    }
+  }
+  return &(t->val[in]);
+}
 
 void ULListStr::set(size_t loc, const std::string& val)
 {
@@ -63,3 +184,4 @@ void ULListStr::clear()
   tail_ = NULL;
   size_ = 0;
 }
+
